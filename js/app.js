92a4abc03e6fe1a428e68a2a0f3f2d5f704e7219
Florencia Amendola <amendolaflorencia@gmail.com>
2021-11-01 01:19:47 -0300

    rendering products

--- js/app.js
+++ js/app.js
@@ -15,3 +15,35 @@
                   $('nav').removeClass('black');
             }
       })
+/// RECORRO EL ARRAY DE TODOS LOS PRODUCTOS
+for(let food of dataFood){
+    crearFichaProducto(food);
+}
+
+
+/// FUNCION PARA RENDERIZAR FICHAS DE CADA PRODUCTO POR MEDIO DEL DOM
+function crearFichaProducto(food){
+	 let cont_father = document.getElementById("contenidoGenerado");
+ 	 let cont_child = document.createElement("div");
+ 	cont_child.innerHTML = `<div class="container">
+                            <div class="card-deck">
+                              <div class="col-lg-8 card-area">
+                                <div class="card" style="width:18rem" id="ficha">
+                                <img src="${food.image}" class="imagenes" alt="Card image cap">  
+                                <div class="card-body text-center">
+                                  <h5> ${food.name} </h5>
+                                   <hr>
+                                  <p> Descripcion: ${food.description}</p>                                                                                                
+                                    <h6>$ ${food.price} </h6>                       
+                                  <button type="button" id=${food.id} class="btn btn-danger btn-sm mr-1 mb-2">
+                                    <i class="fas fa-shopping-cart pr-2"></i>Comprar
+                                  </button>
+                                </div>
+                              </div>
+                            </div>
+                          </div>`
+
+ 		
+	  cont_father.appendChild(cont_child);
+}
+
--- js/data.js
+++ js/data.js
@@ -1,1 +1,60 @@
 ////// data estatica JSON
+
+let dataFood = [{
+  "id": 1,
+  "name": "Frutillas",
+  "description": "Frutillas disecadas super dulces.",
+  "image": "./images/food1.jpg",
+  "price": 250,
+  "count": 1,
+}, {
+  "id": 2,
+  "name": "Biscuit",
+  "description": "Galletas nutri choco y crema.",
+  "image":"./images/food2.jpg",
+  "price": 200,
+  "count": 1,
+}, {
+  "id": 3,
+  "name": "Sopa",
+  "description": "Sopa de verduras con vasito de regalo.",
+  "image": "./images/food3.png",
+  "price": 300,
+  "count": 1,
+}, {
+  "id": 4,
+  "name": "Cookie",
+  "description": "Galletita rellena doble crema.",
+  "image": "./images/food4.jpg",
+  "price": 200,
+  "count": 1,
+}, {
+  "id": 5,
+  "name": "Nutri pure",
+  "description": "Exquisito pure de papas.",
+  "image": "./images/food5.jpg",
+  "price": 180,
+  "count": 1,
+}, {
+  "id": 6,
+  "name": "Cookie Ice",
+  "description": "Tu postre ideal para el espacio!",
+  "image":"./images/food6.jpg",
+  "price": 230,
+  "count": 1,
+}, {
+  "id": 7,
+  "name": "Ciruela",
+  "description": "Ciruelas disecadas en gajos.",
+  "image": "./images/food7.png",
+  "price": 180,
+  "count": 1,
+}, {
+  "id": 8,
+  "name": "Queso",
+  "description": "Fetas de queso de cabra.",
+  "image": "./images/food8.jpg",
+  "price": 270,
+  "count": 1,
+}
+ ]
